2c15a8fb6552f0dd226a9c13277646fb5db572db
anandmoorthy3 <anandhamoorthy@madstreetden.com>
2020-04-10 21:11:28 +0530

    App name changed from devstories to devstory

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "devstories.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "devstory.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
